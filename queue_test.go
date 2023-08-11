e206e3f57a20762f8b21c023697601a64be6608b
juju <chenjun1@chinatelecom.cn>
2023-08-11 10:09:06 +0800

    增加测试用例

--- queue_test.go
+++ queue_test.go
@@ -2,11 +2,25 @@ package queueg
 
 import (
 	"fmt"
+	"strconv"
+	"sync/atomic"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
 )
+
+type ABC struct {
+	A int
+	B int
+	C int
+	D int
+	E int
+	F int
+	G int
+	H int
+	I int
+}
 
 func TestSize(t *testing.T) {
 	q := NewQueue[int](10000)
@@ -33,14 +47,65 @@ func TestPoP(t *testing.T) {
 	assert.Equal(t, int64(0), q.Size())
 }
 
-type ABC struct {
-	A int
-	B int
-	C int
-	D int
-	E int
-	F int
-	G int
-	H int
-	I int
+func TestPoP2(t *testing.T) {
+	total := atomic.Int32{}
+	fn := func(abc *ABC) {
+		total.Add(1)
+	}
+	q := NewQueueWithFn[*ABC](1000, fn)
+	for i := 0; i < 1000; i++ {
+		q.Push(&ABC{i, i, i, i, i, i, i, i, i})
+	}
+	assert.Equal(t, int64(1000), q.Size())
+
+	q.Run()
+	time.Sleep(time.Second * 1)
+	assert.Equal(t, int64(0), q.Size())
+	time.Sleep(time.Second * 1)
+	assert.Equal(t, int32(1000), total.Load())
+}
+
+func BenchmarkPushInt(b *testing.B) {
+	q := NewQueue[int](100000000)
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		q.Push(i)
+	}
+	b.StopTimer()
+}
+
+func BenchmarkPushString(b *testing.B) {
+	q := NewQueue[string](100000000)
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		q.Push(strconv.Itoa(i))
+	}
+	b.StopTimer()
+}
+
+func BenchmarkPushSlice(b *testing.B) {
+	q := NewQueue[[]string](100000000)
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		q.Push([]string{strconv.Itoa(i), strconv.Itoa(i), strconv.Itoa(i)})
+	}
+	b.StopTimer()
+}
+
+func BenchmarkPushMap(b *testing.B) {
+	q := NewQueue[map[string]int](100000000)
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		q.Push(map[string]int{strconv.Itoa(i): i})
+	}
+	b.StopTimer()
+}
+
+func BenchmarkStruct(b *testing.B) {
+	q := NewQueue[*ABC](100000000)
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		q.Push(&ABC{i, i, i, i, i, i, i, i, i})
+	}
+	b.StopTimer()
 }
